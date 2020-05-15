3177644883b476aaf102389ef231574b5461d83f
dev0088 <daniel.m.v.e.89@gmail.com>
2020-05-14 19:46:00 -0700

    Updated docker and docker-compose and nginx config.

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,16 @@ docker-compose down
 # Run dockers as daemon
 docker-compose up -d --build
 
+# Generate key
+docker-compose exec app php artisan key:generate
+
+# Save config files
+docker-compose exec app php artisan config:cache
+
+# Migrate database
+docker-compose exec app php artisan migrate
+
 # Remove caches
-# yes | docker system prune
+yes | docker system prune
 
 
